7f498fc96e0c2cfee0dbceb0d87d0caae496bbf6
Siarhei1927 <shaurylau@gmail.com>
2023-04-17 10:22:52 +0200

    feat: realise stack functions

--- src/stack.js
+++ src/stack.js
@@ -1,31 +1,43 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
- */
+class Node {
+  constructor (element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor () {
+    this.head = null;
+    this.length = 0;
+  }
+
+
+  
+  push(element) {
+    const node = new Node(element);
+      if (this.head) {
+        node.next = this.head;
+        this.head = node;
+      } else {
+        this.head = node;
+      }
+
+      this.length++
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const current = this.head;
+      this.head = this.head.next;
+      this.length--;
+
+      return current.element;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head.element;
   }
 }
 
